7343053def1cc6cde475c4e624dffd9c36836204
tianiue <61725402+tianiue@users.noreply.github.com>
2023-11-20 11:28:34 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -12,3 +12,18 @@
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
 
+git clone https://github.com/tianiue/luci-packages.git package/luci-packages
+git clone https://github.com/tianiue/luci-app-alist.git package/luci-app-alist
+# git clone https://github.com/linkease/nas-packages-luci.git package/nas-packages-luci
+# git clone https://github.com/linkease/nas-packages.git package/nas-packages
+git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall-luci
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
+git clone https://github.com/fw876/helloworld.git package/helloworld
+git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+git clone https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+# git clone https://github.com/jerrykuku/luci-app-ttnode.git package/luci-app-ttnode
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
+# git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
+# git clone https://github.com/lisaac/luci-lib-docker.git package/luci-lib-docker
